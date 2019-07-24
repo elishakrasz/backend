4baf97b69be7e89c4633af95feec83ff73735ab4
Elisha Mallard <emallard@exigentcap.com>
2019-07-24 18:22:22 +0300

    first commit

---
+++ src/modules/user/CreateUser.ts
@@ -0,0 +1,37 @@
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  ClassType,
+  UseMiddleware
+} from "type-graphql";
+import { RegisterInput } from "./register/RegisterInput";
+import { User } from "../../entity/User";
+import { Middleware } from "type-graphql/interfaces/Middleware";
+
+function createResolver<T extends ClassType, X extends ClassType>(
+  suffix: string,
+  returnType: T,
+  inputType: X,
+  entity: any,
+  middleware?: Middleware<any>[]
+) {
+  @Resolver()
+  class BaseResolver {
+    @Mutation(() => returnType, { name: `create${suffix}` })
+    @UseMiddleware(...(middleware || []))
+    async create(@Arg("data", () => inputType) data: any) {
+      return entity.create(data).save();
+    }
+  }
+
+  return BaseResolver;
+}
+
+export const CreateUserResolver = createResolver(
+  "User",
+  User,
+  RegisterInput,
+  User
+);
+
